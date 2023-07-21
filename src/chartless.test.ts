76189e7c5be75b1a3e808dc2d2d886ddae4236a9
eprince1 <eitanaprince@gmail.com>
2023-07-21 11:45:25 -0400

    change to class and start bar chart

--- src/chartless.test.ts
+++ src/chartless.test.ts
@@ -1,74 +1,80 @@
-import * as Chartless from ".";
-import { Point } from "./models/point.model";
+import Chartless from '.';
+import { Point } from './models/point.model';
 
-test("adds 2 points", () => {
+test('adds 2 points', () => {
+  const chartless = new Chartless();
   const p1: Point = { x: 1, y: 2 };
   const p2: Point = { x: 3, y: 4 };
   const answer: Point = { x: 4, y: 6 };
-  expect(Chartless.add(p1, p2)).toStrictEqual(answer);
+  expect(chartless.add(p1, p2)).toStrictEqual(answer);
 });
 
-test("subtracts 2 points", () => {
+test('subtracts 2 points', () => {
+  const chartless = new Chartless();
   const p1: Point = { x: 1, y: 2 };
   const p2: Point = { x: 3, y: 4 };
   const answer: Point = { x: 2, y: 2 };
-  expect(Chartless.sub(p2, p1)).toStrictEqual(answer);
+  expect(chartless.sub(p2, p1)).toStrictEqual(answer);
 });
 
-test("scale a point", () => {
+test('scale a point', () => {
+  const chartless = new Chartless();
   const p1: Point = { x: 1, y: 2 };
   const s: number = 5;
   const answer: Point = { x: 5, y: 10 };
-  expect(Chartless.scale(s, p1)).toStrictEqual(answer);
+  expect(chartless.scale(s, p1)).toStrictEqual(answer);
 });
 
-test("creates a document with an svg element", () => {
-  const [document, svg] = Chartless.generateDocument();
-  expect(document.children.length).toBe(1);
-  expect(svg !== null).toBe(true);
+test('creates a document with an svg element', () => {
+  const chartless = new Chartless();
+  expect(chartless.document.children.length).toBe(1);
+  expect(chartless.svg !== null).toBe(true);
 });
 
-test("creates an svg element and sets attributes", () => {
-  const [document, svg] = Chartless.generateDocument();
-  const path = Chartless.createSvgElement(document, "path");
-  Chartless.setAttributes(path, [{ key: "id", value: "path" }]);
-  svg.appendChild(path);
-  expect(svg.children.length).toBe(1);
-  expect(!!document.getElementById("path")).toBe(true);
+test('creates an svg element and sets attributes', () => {
+  const chartless = new Chartless();
+  const path = chartless.createSvgElement('path');
+  chartless.setAttributes(path, [{ key: 'id', value: 'path' }]);
+  chartless.svg.appendChild(path);
+  expect(chartless.svg.children.length).toBe(1);
+  expect(!!chartless.document.getElementById('path')).toBe(true);
 });
 
-test("random number between 2 points", () => {
+test('random number between 2 points', () => {
   const rand = Chartless.getRandomNumber(1, 100);
   expect(rand).toBeGreaterThanOrEqual(1);
   expect(rand).toBeLessThan(100);
 });
 
-test("generate hex", () => {
+test('generate hex', () => {
   const rand = Chartless.getRandomHexColor();
-  expect(rand.includes("#")).toBe(true);
+  expect(rand.includes('#')).toBe(true);
 });
 
-test("generates line", () => {
+test('generates line', () => {
+  const chartless = new Chartless();
   const point: Point = { x: 0, y: 0 };
-  expect(Chartless.line(point)).toBe("L 0 0");
+  expect(chartless.line(point)).toBe('L 0 0');
 });
 
-test("generates smooth path", () => {
+test('generates smooth path', () => {
+  const chartless = new Chartless();
   const points: Point[] = [
     { x: 0, y: 0 },
     { x: 10, y: 10 },
     { x: 20, y: 20 },
   ];
-  const path = Chartless.pathD(points, "smooth");
-  expect(path).toBe("M 0 0 C 1.5 1.5, 7 7, 10 10 C 13 13, 18.5 18.5, 20 20");
+  const path = chartless.pathD(points, 'smooth');
+  expect(path).toBe('M 0 0 C 1.5 1.5, 7 7, 10 10 C 13 13, 18.5 18.5, 20 20');
 });
 
-test("generates line path", () => {
+test('generates line path', () => {
+  const chartless = new Chartless();
   const points: Point[] = [
     { x: 0, y: 0 },
     { x: 10, y: 10 },
     { x: 20, y: 20 },
   ];
-  const path = Chartless.pathD(points, "line");
-  expect(path).toBe("M 0 0 L 10 10 L 20 20");
+  const path = chartless.pathD(points, 'line');
+  expect(path).toBe('M 0 0 L 10 10 L 20 20');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,142 @@
-import { Point } from './models/point.model';
+import { GraphType } from './models/graph.model';
+import { DataPoint, Point } from './models/point.model';
 import { JSDOM } from 'jsdom';
 
-export function generateDocument(): [Document, SVGSVGElement] {
-  const document = new JSDOM().window.document;
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-  return [document, document.body.appendChild(svg)];
-}
+export default class Chartless {
+  document: Document;
+  svg: SVGElement;
 
-export function createSvgElement(document: Document, namespace: string): SVGElement {
-  return document.createElementNS('http://www.w3.org/2000/svg', namespace);
-}
+  constructor() {
+    this.document = new JSDOM().window.document;
+    this.svg = this.createSvgElement('svg');
+    this.svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    this.document.body.appendChild(this.svg);
+  }
 
-export function setAttributes(
-  element: Element,
-  attributes: { key: string; value: string }[],
-): void {
-  attributes.forEach((attr) => element.setAttribute(attr.key, attr.value));
-}
+  createSvgElement(namespace: string): SVGElement {
+    return this.document.createElementNS('http://www.w3.org/2000/svg', namespace);
+  }
 
-export function line(point: Point): string {
-  return `L ${point.x} ${point.y}`;
-}
+  setAttributes(element: Element, attributes: { key: string; value: string }[]): void {
+    attributes.forEach((attr) => element.setAttribute(attr.key, attr.value));
+  }
 
-export function smooth(_: Point, i: number, points: Point[]): string {
-  const smoothing = 0.15;
-  const pStart = points[i - 1];
-  const pEnd = points[i];
+  line(point: Point): string {
+    return `L ${point.x} ${point.y}`;
+  }
 
-  const pPrev = points[i - 2] || pStart;
-  const pNext = points[i + 1] || pEnd;
+  smooth(_: Point, i: number, points: Point[]): string {
+    const smoothing = 0.15;
+    const pStart = points[i - 1];
+    const pEnd = points[i];
 
-  const controlStart = add(pStart, scale(smoothing, sub(pEnd, pPrev)));
-  const controlEnd = add(pEnd, scale(smoothing, sub(pStart, pNext)));
+    const pPrev = points[i - 2] || pStart;
+    const pNext = points[i + 1] || pEnd;
 
-  return `C ${controlStart.x} ${controlStart.y}, ${controlEnd.x} ${controlEnd.y}, ${pEnd.x} ${pEnd.y}`;
-}
+    const controlStart = this.add(pStart, this.scale(smoothing, this.sub(pEnd, pPrev)));
+    const controlEnd = this.add(pEnd, this.scale(smoothing, this.sub(pStart, pNext)));
 
-export function pathD(points: Point[], command: 'line' | 'smooth'): string {
-  return points.reduce(
-    (d, point, i, a) =>
-      i === 0
-        ? `M ${point.x} ${point.y}`
-        : `${d} ${command === 'line' ? line(point) : smooth(point, i, a)}`,
-    '',
-  );
-}
+    return `C ${controlStart.x} ${controlStart.y}, ${controlEnd.x} ${controlEnd.y}, ${pEnd.x} ${pEnd.y}`;
+  }
 
-export function add(pointA: Point, pointB: Point): Point {
-  return { x: pointA.x + pointB.x, y: pointA.y + pointB.y };
-}
+  pathD(points: Point[], command: 'line' | 'smooth'): string {
+    return points.reduce(
+      (d, point, i, a) =>
+        i === 0
+          ? `M ${point.x} ${point.y}`
+          : `${d} ${command === 'line' ? this.line(point) : this.smooth(point, i, a)}`,
+      '',
+    );
+  }
 
-export function sub(pointA: Point, pointB: Point): Point {
-  return { x: pointA.x - pointB.x, y: pointA.y - pointB.y };
-}
+  continuePathD(points: Point[], command: 'line' | 'smooth'): string {
+    return points.reduce(
+      (d, point, i, a) =>
+        i === 0 ? `` : `${d} ${command === 'line' ? this.line(point) : this.smooth(point, i, a)}`,
+      '',
+    );
+  }
 
-export function scale(s: number, point: Point): Point {
-  return { x: s * point.x, y: s * point.y };
-}
+  add(pointA: Point, pointB: Point): Point {
+    return { x: pointA.x + pointB.x, y: pointA.y + pointB.y };
+  }
 
-export function getRandomNumber(min: number, max: number): number {
-  const randomDecimal = Math.random();
-  const randomNumber = Math.floor(randomDecimal * max) + min;
+  sub(pointA: Point, pointB: Point): Point {
+    return { x: pointA.x - pointB.x, y: pointA.y - pointB.y };
+  }
 
-  return randomNumber;
-}
+  scale(s: number, point: Point): Point {
+    return { x: s * point.x, y: s * point.y };
+  }
 
-export function getRandomHexColor(): string {
-  const randomNum = Math.floor(Math.random() * 16777216);
-  const hexColor = randomNum.toString(16).padStart(6, '0');
-  return `#${hexColor}`;
+  midpoint(pointA: Point, pointB: Point): Point {
+    return { x: (pointA.x + pointB.x) / 2, y: (pointA.y + pointB.y) / 2 };
+  }
+
+  static getRandomNumber(min: number, max: number): number {
+    const randomDecimal = Math.random();
+    const randomNumber = Math.floor(randomDecimal * max) + min;
+
+    return randomNumber;
+  }
+
+  static getRandomHexColor(): string {
+    const randomNum = Math.floor(Math.random() * 16777216);
+    const hexColor = randomNum.toString(16).padStart(6, '0');
+    return `#${hexColor}`;
+  }
+
+  appendToNode(namespace: string, node: SVGElement = this.svg): SVGElement {
+    const svgElement = this.createSvgElement(namespace);
+    return node.appendChild(svgElement);
+  }
+
+  createBarGraph({
+    data,
+    rounding = 0,
+    width = 800,
+  }: {
+    data: DataPoint[];
+    rounding?: number;
+    width?: number;
+  }): void {
+    const g = this.appendToNode('g');
+    const maxH = Math.ceil(Math.max(...data.map((d) => d.value)) / 100) * 100;
+    const scale = 500;
+    const barWidth = width / data.length;
+    const gutter = barWidth * 0.2;
+
+    const color = Chartless.getRandomHexColor();
+
+    data.forEach((dp, i) => {
+      const value = ((maxH - dp.value) / maxH) * scale;
+      const line = this.appendToNode('path', g);
+      const basePoint: Point = { y: scale, x: i * barWidth + gutter };
+      const topLeftPoint: Point = {
+        y: Math.min(value + rounding, scale),
+        x: i * barWidth + gutter,
+      };
+      const topRightPoint: Point = {
+        y: Math.min(value + rounding, scale),
+        x: i * barWidth + barWidth - gutter,
+      };
+      const endPoint: Point = { y: scale, x: i * barWidth + barWidth - gutter };
+      let path = this.pathD([basePoint, topLeftPoint], 'line');
+      const tlA = this.add(topLeftPoint, { x: rounding, y: -rounding });
+      const trA = this.add(topRightPoint, { x: -rounding, y: -rounding });
+      path += ` A ${rounding} ${rounding} 0 0 1 ${tlA.x} ${tlA.y}`;
+      path += this.continuePathD([tlA, trA], 'line');
+      path += ` A ${rounding} ${rounding} 0 0 1 ${topRightPoint.x} ${topRightPoint.y}`;
+      path += this.continuePathD([topRightPoint, endPoint, basePoint], 'line');
+
+      line.setAttribute('d', path);
+      line.setAttribute('fill', color);
+      line.setAttribute('stroke', color);
+      line.setAttribute('stroke-width', '1');
+    });
+  }
+
+  svgHTML(): string {
+    return this.svg.outerHTML;
+  }
 }
